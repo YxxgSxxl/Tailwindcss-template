fef35afb6e8990d644ea22f83236ad47988583b7
Alex <114391274+YxxgSxxl@users.noreply.github.com>
2024-04-05 09:06:23 +0200

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'orange': "rgb(255 174 0 / <alpha-value>)",
+        'green': "rgb(55 179 74 / <alpha-value>)",
+        'black': "rgb(26 25 25 / <alpha-value>)",
+        'grey': "rgb(69 66 65 / <alpha-value>)",
+        'white': 'rgb(255 255 255 / <alpha-value>)',
+      }
+    },
   },
   plugins: [],
 }
